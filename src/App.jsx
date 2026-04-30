6aef0f010f99d92d8c6b88837cbc21b492f47ab8
Bob <bob@buildthehomelab.com>
2026-04-30 16:09:19 -0400

    auto: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useMemo } from 'react';
 import Dozzle from './components/Dozzle';
-import TrueNas from './components/TrueNas';
+import TrueNas, { useTrueNas, nasIssues } from './components/TrueNas';
 import {
   useTweaks, TweaksPanel, TweakSection, TweakRadio, TweakToggle,
 } from './components/TweaksPanel';
