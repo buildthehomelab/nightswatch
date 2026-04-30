03bd56c4acb86ddc6e25424b848bc4550df08fb3
Bob <bob@buildthehomelab.com>
2026-04-30 16:06:07 -0400

    auto: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/truenas': {
+        target: 'https://patronus.vaultrona.com:3443',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/truenas/, ''),
+        secure: false,
+      },
+    },
+  },
 })
