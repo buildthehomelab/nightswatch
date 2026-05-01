b1e43ed0944a33f56a69cdac3615d1426820adee
Bob <bob@buildthehomelab.com>
2026-04-30 22:45:36 -0400

    auto: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -544,11 +544,11 @@ export default function App() {
           ]}
           onChange={(v) => setTweak("ambientPlacement", v)}
         />
-        <TweakToggle label="Weather"  value={t.showWeather} onChange={(v) => setTweak("showWeather", v)} />
-        <TweakToggle label="WAN"      value={t.showWan}     onChange={(v) => setTweak("showWan", v)} />
-        <TweakToggle label="Uptime"   value={t.showUptime}  onChange={(v) => setTweak("showUptime", v)} />
-        <TweakToggle label="TrueNAS"  value={t.showNas}     onChange={(v) => setTweak("showNas", v)} />
         <TweakToggle label="Date"     value={t.showDate}    onChange={(v) => setTweak("showDate", v)} />
+        <TweakToggle label="TrueNAS"  value={t.showNas}     onChange={(v) => setTweak("showNas", v)} />
+        <TweakToggle label="Uptime"   value={t.showUptime}  onChange={(v) => setTweak("showUptime", v)} />
+        <TweakToggle label="WAN"      value={t.showWan}     onChange={(v) => setTweak("showWan", v)} />
+        <TweakToggle label="Weather"  value={t.showWeather} onChange={(v) => setTweak("showWeather", v)} />
       </TweaksPanel>
     </>
   );
