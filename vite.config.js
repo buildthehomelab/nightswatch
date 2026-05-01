1933e15ad0d6cc627a81cf945a3a21703cc3a5fc
Bob <bob@buildthehomelab.com>
2026-05-01 14:09:21 -0400

    auto: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -63,6 +63,7 @@ export default defineConfig(({ mode }) => {
       'import.meta.env.VITE_STOPPED_APP_HIDE_MINUTES': JSON.stringify(env.TRUENAS_STOPPED_APP_HIDE_MINUTES ?? ''),
       'import.meta.env.VITE_DOZZLE_URL':               JSON.stringify(env.DOZZLE_URL ?? ''),
       'import.meta.env.VITE_WEATHER_LOCATION':         JSON.stringify(env.WEATHER_LOCATION ?? ''),
+      'import.meta.env.VITE_DEMO':                     JSON.stringify(env.DEMO ?? 'false'),
     },
     plugins: [react(), basicSsl(), truenasProxyPlugin(env.TRUENAS_KEY, env.TRUENAS_HOST, env.TRUENAS_PORT)],
     server: {
