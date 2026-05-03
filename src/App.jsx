0091b813f4625edd90c81f5c010bd49ebcb2c271
Bob <bob@buildthehomelab.com>
2026-05-02 23:39:31 -0400

    auto: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -770,6 +770,7 @@ export default function App() {
           wanUp={wanUp}
           uptime={uptime}
           rank={rank}
+          cleanSince={cleanSince}
           weather={weather}
           showWeather={t.showWeather}
           showWan={t.showWan}
