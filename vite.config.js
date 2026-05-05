dcf2b2e6396bd8842522cd3c95c26bd3b36e463d
Bob <bob@buildthehomelab.com>
2026-05-05 14:11:11 -0400

    chore: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import https from 'node:https'
+import http from 'node:http'
 import fs from 'node:fs'
 
 const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
